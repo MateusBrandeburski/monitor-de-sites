7d47f4b6dbff6b815354338d0015c814d74fb8d4
Mateus <mateus.brandeburski92@mail.com>
2023-07-07 16:47:28 -0300

    :sparkles: Sites monitorados em arquivo de texto

--- src/moniorador-de-sites.go
+++ src/moniorador-de-sites.go
@@ -1,23 +1,30 @@
 package main
 
-import (  
-    "fmt"
-    "os"
-    "net/http"
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+	"time"
 )
+
+const monitoramentos = 5
+const delay = 1 
 
 func main() {
 
 	exibeIntroducao()
 	for {
-
 		exibirMenu()
 		comando := leComando()
 		switch comando {
-		case 1: 
-			iniciarMonitoramento()
+		case 1:
+			iniciarMonitoramento(leSitesDoArquivo())
 		case 2:
 			fmt.Println("Exibindo Logs...")
+			
 		case 0:
 			fmt.Println("Saindo do programa")
 			os.Exit(0) // libera espaço na memória???
@@ -35,43 +42,71 @@ func exibeIntroducao() {
     fmt.Println("Este programa está na versão", versao)
 }
 
-func exibirMenu () {
-    
+func exibirMenu() {
+	fmt.Print("\n")
     fmt.Println("1- Iniciar Monitoramento")
     fmt.Println("2- Exibir Logs")
     fmt.Println("0- Sair do Programa")
+	fmt.Print("\n")
 }
 
 func leComando() int {
     var comandoLido int
+	fmt.Print("Digite o comando: ")
     fmt.Scan(&comandoLido)
     fmt.Println("O Comando escolhido foi", comandoLido)
 
     return comandoLido
 }
 
-func iniciarMonitoramento () {
+func iniciarMonitoramento(sites []string) {
 
     fmt.Println("Monitorando...")
 
-	sites := []string{"https://alura.com.br","https://web-production-c42c.up.railway.app/", "https://api.cryptoscamdb.org/"}
-
-    for _, site := range(sites) {	
-
-		resp, _ := http.Get(site)
-
-		if resp.StatusCode == 200 {
-			fmt.Println("Site:", site, "Carregado com Sucesso! Status Code:", resp.StatusCode)
-		} else if resp.StatusCode == 404 {
-			fmt.Println("Site:", site, "não encontrado ou fora do ar")
-		} else {
-			fmt.Println("Site:", site, "está com problemas. Status Code:", resp.StatusCode)
-		} 
-	}  
+	// sites := 
+	cont := 0
+	
+	for i:=0 ; i < monitoramentos ; i++ {
+		cont++
+		fmt.Println("Número da tentiva:",cont)
+		for _, site := range(sites) {
+			testaSite(site)
+		}
+		time.Sleep(delay * time.Second)  
+	}
 }
 
-// func exibiNomes() {
+func testaSite(site string) {
 
-// 	nomes := []string{"Douglas", "Mateus", "Enzo"}
-// 	fmt.Println(nomes)
-// }
+	resp, _ := http.Get(site)
+
+	if resp.StatusCode == 200 {
+		fmt.Println("Site:", site, "Carregado com Sucesso! Status Code:", resp.StatusCode)
+	} else if resp.StatusCode == 404 {
+		fmt.Println("Site:", site, "não encontrado ou fora do ar")
+	} else {
+		fmt.Println("Site:", site, "está com problemas. Status Code:", resp.StatusCode)
+	} 
+
+}
+
+func leSitesDoArquivo() []string {
+	var sites []string
+
+	arquivo, _ := os.Open("sites-monitorados.txt")
+	leitor := bufio.NewReader(arquivo)
+
+	for {
+		linha, err := leitor.ReadString('\n')
+		linha = strings.TrimSpace(linha)
+
+		sites = append(sites, linha)
+		if err == io.EOF {
+			break
+		}
+		
+	}
+	arquivo.Close() //liberar o arquivo para outros programas do SO poderem usar.
+	return sites
+}	
+
